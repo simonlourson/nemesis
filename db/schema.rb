6defa3e59d9f6a7577c0e8711fd4203472b8a408
louis-mazet <louis.mazet@doctolib.com>
2022-03-09 17:16:27 +0100

    create game

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_03_09_160241) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_09_161133) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
+
+  create_table "games", id: :uuid, default: -> { "gen_random_uuid()" }, force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
